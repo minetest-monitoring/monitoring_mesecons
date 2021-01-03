11c97f8fe33dc13060ec9ef7255898e261c63560
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-03 17:16:19 +0100

    return on_timer() result

--- luac.lua
+++ luac.lua
@@ -30,15 +30,17 @@ for d = 0, 1 do
 	if def then
 		local old_on_timer = def.on_timer
 
+		-- intercept on_timer call to measure time usage
 		def.on_timer = function(...)
 			local t0 = minetest.get_us_time()
-			old_on_timer(...)
+			local result = old_on_timer(...)
 			local t1 = minetest.get_us_time()
 			local diff = t1 -t0
 
 			metric_time_max.setmax(diff)
 			metric_time.inc(diff)
 			metric_count.inc(1)
+			return result
 		end
 	end
 end
